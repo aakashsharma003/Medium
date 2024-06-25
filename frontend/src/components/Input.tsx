9d652069a37c6d09ef9b0debd967a9072b2f291e
aakashsharma003 <aakash6263264@gmail.com>
2024-06-25 11:01:29 +0530

    adding classes to input

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -4,6 +4,6 @@ interface Inputprops{
 export const Input = ({type,label, placeholder}:Inputprops ) => {
     return <div className="w-[100%]  text-black flex flex-col m-1 ">
      <label htmlFor={label} className="font-bold">{label}</label>
-     <input type={type} placeholder={placeholder} id={label} name={label} className="p-[1rem] md:p-[0.5rem] border border-gray-200 rounded-sm md:rounded-md" />
+     <input type={type} placeholder={placeholder} id={label}  name={label} className="input p-[1rem] md:p-[0.5rem] border border-gray-200 rounded-sm md:rounded-md" />
     </div>
 }
